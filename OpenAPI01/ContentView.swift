1552dbb18659d125000ed24aadf9a4a85acf228f
chlee85095 <chlee85095@cnspartner.com>
2024-03-25 09:38:15 +0900

    Initial Commit

---
+++ OpenAPI01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenAPI01
+//
+//  Created by C.H Lee on 2024.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenAPI01/OpenAPI01App.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAPI01App.swift
+//  OpenAPI01
+//
+//  Created by C.H Lee on 2024.03.25.
+//
+
+import SwiftUI
+
+@main
+struct OpenAPI01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
